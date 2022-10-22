392b490ecf16fa42e7882d8ed79a3f2779dbeb5d
Vasilya <lady_drakula_94@mail.ru>
2022-10-23 00:00:07 +0500

    Ex Задача решена. Результат нужно преобразовать обратно в текст

--- Ex/Program.cs
+++ Ex/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string text = "1qaz";
+string newtext = string.Empty;
+char[] numbers = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', };
+char[] chartext = text.ToCharArray();
+
+for (int i = 0; i < chartext.Length; i++)
+{
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        if (chartext[i] == numbers[j]) chartext[i] = ' ';
+
+    }
+}
+Console.WriteLine(chartext);
+
